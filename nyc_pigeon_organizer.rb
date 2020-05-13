fd25bf38cf264f0144f84948122889ac0ac96026
Nicole Wang <nicole.wang@yale.edu>
2020-05-13 22:18:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized = {}
+  
+  data.each do |cgl, cgl_descriptions|
+    cgl_descriptions.each do |description, array|
+      array.each do |pigeon_name|
+        if organized[pigeon_name] == nil
+          organized[pigeon_name] = {}
+        end
+        if organized[pigeon_name][cgl] == nil
+          organized[pigeon_name][cgl] = []
+        end
+        organized[pigeon_name][cgl].push(description.to_s)
+       end 
+     end
+   end 
+   return organized
 end
